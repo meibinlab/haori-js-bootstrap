01da1df5967e9e4cb83af932e5da9be6da44c8fb
nishijima <nishijima@meibinlab.jp>
2026-09-11 21:26:00 +0900

    0.5.43

--- demo/cdn.js
+++ demo/cdn.js
@@ -19,7 +19,7 @@ const haoriBootstrapVersion =
   typeof haoriBootstrap?.version === "string" ? haoriBootstrap.version : null;
 const isReady = Boolean(
   haoriBootstrap &&
-  haoriBootstrapVersion === "0.5.42" &&
+  haoriBootstrapVersion === "0.5.43" &&
   typeof haoriBootstrap.isInstalled === "function" &&
   haoriBootstrap.isInstalled() &&
   haori &&
